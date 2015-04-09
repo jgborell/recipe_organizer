8d9fbaba2256ff5b9454e1dc10b271b13c52182c
John Borell <assaltshaker@yahoo.com>
2015-04-09 13:39:40 -0600

    clearing issues from last push

--- backend/apps/recipes/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Recipe(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    directions = models.TextField()
-
-    def __str__(self):
-        return self.name
