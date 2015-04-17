8e7fd4e6432d96cbc9c501c70884d7a087e3a1b9
John Borell <assaltshaker@yahoo.com>
2015-04-17 13:43:51 -0600

    added recipe detail and add recipe features

---
+++ frontend/app/add-recipe/addRecipe.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('myApp.addRecipe', ['ngRoute'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/add-recipe', {
+            templateUrl: 'add-recipe/add-recipe.html',
+            controller: 'AddRecipeCtrl'
+        });
+    }])
+
+    .controller('AddRecipeCtrl', ['$scope', 'Restangular', function ($scope, Restangular) {
+        $scope.addRecipe = function () {
+            Restangular.all('add-recipe').customPOST($scope.recipe).then(function () {
+                alert("Recipe was successfully created!");
+                $scope.recipe = {};
+            }, function () {
+                alert("There was a problem adding your recipe");
+            });
+        };
+    }]);
