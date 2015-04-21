36242220363feba38293b44a09428705793710f5
John Borell <assaltshaker@yahoo.com>
2015-04-21 16:44:24 -0600

    css tweaks and added ingredient editing options

--- frontend/app/add-recipe/addRecipe.js
+++ frontend/app/add-recipe/addRecipe.js
@@ -21,12 +21,18 @@ angular.module('myApp.addRecipe', ['ngRoute'])
                 $scope.ingredientName = null;
             }
         };
+        $scope.removeIngredientFromRecipe = function(ingredient) {
+            var index = $scope.recipe.ingredients.indexOf(ingredient);
+            if (index != -1) {
+                $scope.recipe.ingredients.splice(index, 1);
+            }
+        };
         $scope.addRecipe = function () {
             Restangular.all('add-recipe').customPOST($scope.recipe).then(function () {
-                alert("Recipe was successfully created!");
+                toastr.success("Recipe was successfully created!");
                 $scope.recipe = {};
             }, function () {
-                alert("There was a problem adding your recipe");
+                toastr.error("There was a problem adding your recipe");
             });
         };
     }]);
