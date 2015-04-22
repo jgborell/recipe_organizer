bd0a426e40397fd38374ec89b8d755eca6b3e4d0
John Borell <assaltshaker@yahoo.com>
2015-04-22 14:35:09 -0600

    added frontend add photo capabilities

--- backend/apps/recipes/models.py
+++ backend/apps/recipes/models.py
@@ -13,6 +13,7 @@ class Recipe(models.Model):
     description = models.TextField()
     directions = models.TextField()
     ingredients = models.ManyToManyField(Ingredient)
+    photo = models.ImageField(upload_to='photos', blank=True, null=True)
 
     def __str__(self):
         return self.name
